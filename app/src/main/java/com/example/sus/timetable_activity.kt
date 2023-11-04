8c21a24d9d579d9cd3ee1758b3dc8b54dfa2410f
Merama <lych.ref@gmail.com>
2023-11-04 10:37:05 +0300

    доделал первую версию расписания, осталось привязать к календарю

--- app/src/main/java/com/example/sus/timetable_activity.kt
+++ app/src/main/java/com/example/sus/timetable_activity.kt
@@ -5,10 +5,12 @@ import android.annotation.SuppressLint
 import androidx.core.content.ContextCompat
 import androidx.core.widget.TextViewCompat
 import android.content.Intent
+import android.content.Context
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
 import android.widget.CalendarView
+import android.view.ViewGroup
 import android.graphics.Color
 import android.widget.CalendarView.OnDateChangeListener
 import android.widget.TableLayout
@@ -25,6 +27,7 @@ class timetable_activity : AppCompatActivity() {
     private lateinit var dateTV: TextView
     private lateinit var calendarView: CalendarView
     private lateinit var tableLayout: TableLayout
+
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,8 +70,12 @@ class timetable_activity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun populateTimeTable(studentTimeTable: List<StudentTimeTable>?) {
         tableLayout.removeAllViews()
+        val paddingInPixels = 5
+        val paddingInDp = paddingInPixels.convertPixelsToDp(this)
+        val fontSize = 14f
 
         studentTimeTable?.let { timeTableList ->
             var prevGroupName = ""
@@ -83,7 +90,7 @@ class timetable_activity : AppCompatActivity() {
                     val separatorRow = TableRow(this)
                     val separatorCell = TextView(this)
                     separatorCell.text = ""
-                    separatorCell.setPadding(0, 10, 0, 10)
+                    separatorCell.setPadding(0, paddingInDp * 2, 0, paddingInDp * 2)
                     separatorRow.addView(separatorCell)
                     tableLayout.addView(separatorRow)
                     prevGroupName = groupName
@@ -100,10 +107,9 @@ class timetable_activity : AppCompatActivity() {
 
                 val groupCell = TextView(this)
                 groupCell.text = " (${groupName}) \n"
-                groupCell.setTextColor(ContextCompat.getColor(this, R.color.black))
-                groupCell.setPadding(5, 5, 5, 5)
-                groupCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
-                groupCell.setBackgroundResource(R.color.white)
+                groupCell.setTextColor(ContextCompat.getColor(this, R.color.white))
+                groupCell.setPadding(paddingInDp, paddingInDp, paddingInDp, paddingInDp)
+                groupCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize)
 
                 val groupCellLayoutParams = TableRow.LayoutParams(
                     TableRow.LayoutParams.WRAP_CONTENT,
@@ -115,19 +121,20 @@ class timetable_activity : AppCompatActivity() {
 
                 val facultyCell = TextView(this)
                 facultyCell.text = studentTimeTable.facultyName
-                facultyCell.layoutParams = TableRow.LayoutParams(
-                    TableRow.LayoutParams.MATCH_PARENT,
-                    TableRow.LayoutParams.WRAP_CONTENT
-                )
-                facultyCell.setPadding(5, 5, 5, 5)
-                facultyCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13f)
-                facultyCell.setSingleLine(false)
-                facultyCell.setEllipsize(null)
+                facultyCell.setTextColor(ContextCompat.getColor(this, R.color.white))
+                facultyCell.setPadding(paddingInDp, paddingInDp, paddingInDp, paddingInDp)
+                facultyCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize)
+                facultyCell.setMaxWidth(this)
 
-                TextViewCompat.setAutoSizeTextTypeWithDefaults(facultyCell, TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM)
+                TextViewCompat.setAutoSizeTextTypeWithDefaults(
+                    facultyCell,
+                    TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM
+                )
+
+
+                groupRow.setBackgroundResource(R.color.violet1)
 
                 groupRow.addView(facultyCell)
-
 
                 tableLayout.addView(groupRow)
 
@@ -139,8 +146,8 @@ class timetable_activity : AppCompatActivity() {
                         val emptyCell = TextView(this)
                         emptyCell.text = lesson.number.toString()
                         emptyCell.setTextColor(ContextCompat.getColor(this, R.color.white))
-                        emptyCell.setPadding(5, 5, 5, 5)
-                        emptyCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13f)
+                        emptyCell.setPadding(paddingInDp, paddingInDp, paddingInDp, paddingInDp)
+                        emptyCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize)
                         emptyCell.setBackgroundResource(R.color.violet1)
 
                         val emptyCellLayoutParams = TableRow.LayoutParams(
@@ -159,8 +166,8 @@ class timetable_activity : AppCompatActivity() {
                             TableRow.LayoutParams.MATCH_PARENT,
                             TableRow.LayoutParams.WRAP_CONTENT
                         )
-                        emptyTitleCell.setPadding(5, 5, 5, 5)
-                        emptyTitleCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13f)
+                        emptyTitleCell.setPadding(paddingInDp, paddingInDp, paddingInDp, paddingInDp)
+                        emptyTitleCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize)
 
                         emptyRow.addView(emptyTitleCell)
 
@@ -181,8 +188,8 @@ class timetable_activity : AppCompatActivity() {
                                 val emptyCell = TextView(this)
                                 emptyCell.text = i.toString()
                                 emptyCell.setTextColor(ContextCompat.getColor(this, R.color.white))
-                                emptyCell.setPadding(5, 5, 5,                            5)
-                                emptyCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13f)
+                                emptyCell.setPadding(paddingInDp, paddingInDp, paddingInDp, paddingInDp)
+                                emptyCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize)
                                 emptyCell.setBackgroundResource(R.color.violet1)
 
                                 val emptyCellLayoutParams = TableRow.LayoutParams(
@@ -201,8 +208,8 @@ class timetable_activity : AppCompatActivity() {
                                     TableRow.LayoutParams.MATCH_PARENT,
                                     TableRow.LayoutParams.WRAP_CONTENT
                                 )
-                                emptyTitleCell.setPadding(5, 5, 5, 5)
-                                emptyTitleCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13f)
+                                emptyTitleCell.setPadding(paddingInDp, paddingInDp, paddingInDp, paddingInDp)
+                                emptyTitleCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize)
 
                                 emptyRow.addView(emptyTitleCell)
 
@@ -212,11 +219,10 @@ class timetable_activity : AppCompatActivity() {
 
                         val idCell = TextView(this)
                         idCell.text = "${lesson.number.toString()}\n"
-                        idCell.setBackgroundResource(R.drawable.table_border)
+                        idCell.setBackgroundResource(R.drawable.timetable_table_id_cell_border)
                         idCell.setTextColor(ContextCompat.getColor(this, R.color.white))
-                        idCell.setPadding(5, 5, 5, 5)
-                        idCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13f)
-                        idCell.setBackgroundResource(R.color.violet1)
+                        idCell.setPadding(paddingInDp, paddingInDp, paddingInDp, paddingInDp)
+                        idCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize)
 
                         val idCellLayoutParams = TableRow.LayoutParams(
                             TableRow.LayoutParams.WRAP_CONTENT,
@@ -228,17 +234,18 @@ class timetable_activity : AppCompatActivity() {
                         lessonIndex++
 
                         val titleCell = TextView(this)
-                        titleCell.setSingleLine(false)
-                        titleCell.setEllipsize(TextUtils.TruncateAt.END)
-                        titleCell.text = "${discipline.title}\n[к.${discipline.auditorium.campusId.toString()[0]} ${discipline.auditorium.number}] (${discipline.teacher.fio})"
+                        titleCell.text =
+                            " ${discipline.title}\n [к.${discipline.auditorium.campusId.toString()[0]} ${discipline.auditorium.number}] (${discipline.teacher.userName})"
                         titleCell.layoutParams = TableRow.LayoutParams(
                             TableRow.LayoutParams.MATCH_PARENT,
                             TableRow.LayoutParams.WRAP_CONTENT
                         )
-                        titleCell.setPadding(5, 5, 5, 5)
-                        titleCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13f)
+                        titleCell.setPadding(paddingInDp, paddingInDp, paddingInDp, paddingInDp)
+                        titleCell.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize)
+                        titleCell.setMaxWidth(this)
 
                         lessonRow.addView(titleCell)
+
 
                         tableLayout.addView(lessonRow)
 
@@ -249,10 +256,17 @@ class timetable_activity : AppCompatActivity() {
         }
     }
 
+    private fun Int.convertPixelsToDp(context: Context): Int {
+        val scale = context.resources.displayMetrics.density
+        return (this / scale + 0.5f).toInt()
+    }
 
-
-
-
-
+    fun TextView.setMaxWidth(context: Context) {
+        val maxWidth = (context.resources.displayMetrics.widthPixels * 0.75).toInt()
+        this.maxWidth = maxWidth
+    }
 
 }
+
+
+
